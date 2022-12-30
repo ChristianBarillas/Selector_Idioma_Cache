6771484e00d74bd7a7d5e7a1daae498cfc5023c4
christianbarillas <christianbarillasyt@gmail.com>
2022-12-29 20:24:29 -0600

    bug fixed

--- javascript.js
+++ javascript.js
@@ -1,14 +1,36 @@
-"use strict"
-
+"use strict";
+const modal = document.querySelector(".modal_over");
 
 const definiridioma = ()=>{
 document.querySelector(".en").addEventListener("click", ()=> {
 localStorage.setItem("idioma", "en");
 
+cerrarmodal ()
+
 })
 
 document.querySelector(".es").addEventListener("click", ()=>{
 localStorage.setItem("idioma", "es");
+
+cerrarmodal()
+
 })
 
 }
+
+const cerrarmodal = ()=> {
+
+ 
+    modal.style.animation = "desaparecer 1s forwards";
+    setTimeout(()=> modal.style.display= "none", 1000);
+
+
+}
+const idioma = localStorage.getItem("idioma");
+
+if (idioma === null) definiridioma();
+else {
+    modal.style.display = "none";
+}
+
+
